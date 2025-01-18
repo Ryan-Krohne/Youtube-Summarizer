c994a675acb81ccca0a658493383a0b7bcf0d9d8
Ryan Krohne <rkrohnesoccer@gmail.com>
2025-01-18 11:56:52 -0500

    updated api url

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', async (e) => {
     button.textContent = 'Getting Summary...';
 
     try {
-        const response = await fetch('https://renderbackend-8hhs.onrender.com/summarize', {
+        const response = await fetch('https://renderbackend-xfh6.onrender.com/summarize', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
