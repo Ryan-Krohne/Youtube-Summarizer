91f2e5f7a2efa95584049eb58f3d26f6e7eee790
Ryan Krohne <rkrohnesoccer@gmail.com>
2025-05-11 00:49:05 -0400

    renamed api

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ form.addEventListener('submit', async (e) => {
     }
 
     try {
-        const response = await fetch('https://renderbackend-xfh6.onrender.com/testing', {
+        const response = await fetch('https://renderbackend-xfh6.onrender.com/summarize', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
